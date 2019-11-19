57e943ee07511bf69edd8eccb06fb017d2c51c14
husby036 <husby036@umn.edu>
2019-11-19 13:13:12 -0600

    Fix line indent

--- jobscripts/body.sh
+++ jobscripts/body.sh
@@ -39,73 +39,73 @@ fi
 ## Print job preamble
 set +u
 if [ "$scheduler" == "PBS" ]; then
-  echo ________________________________________________________
-  echo
-  echo PBS Job Log
-  echo Start time: $(date)
-  echo
-  echo Job name: $PBS_JOBNAME
-  echo Job ID: $PBS_JOBID
-  echo Submitted by user: $USER
-  echo User effective group ID: $(id -ng)
-  echo
-  echo Hostname of submission: $PBS_O_HOST
-  echo Submitted to cluster: $PBS_SERVER
-  echo Submitted to queue: $PBS_QUEUE
-  echo Requested nodes per job: $PBS_NUM_NODES
-  echo Requested cores per node: $PBS_NUM_PPN
-  echo Requested cores per job: $PBS_NP
-  #echo Node list file: $PBS_NODEFILE
-  #echo Nodes assigned to job: $(cat $PBS_NODEFILE)
-  #echo Running node index: $PBS_O_NODENUM
-  echo
-  echo Running on hostname: $HOSTNAME
-  echo Parent PID: $PPID
-  echo Process PID: $$
-  echo Number of physical cores: $(grep "^core id" /proc/cpuinfo | sort -u | wc -l)
-  echo Number of virtual cores: $(nproc --all)
-  mem_report_cmd="free -g"
-  echo "Memory report [GB] ('${mem_report_cmd}'):"
-  echo -------------------------------------------------------------------------
-  free -g
-  echo -------------------------------------------------------------------------
-  echo
-  working_dir=$PBS_O_WORKDIR
+    echo ________________________________________________________
+    echo
+    echo PBS Job Log
+    echo Start time: $(date)
+    echo
+    echo Job name: $PBS_JOBNAME
+    echo Job ID: $PBS_JOBID
+    echo Submitted by user: $USER
+    echo User effective group ID: $(id -ng)
+    echo
+    echo Hostname of submission: $PBS_O_HOST
+    echo Submitted to cluster: $PBS_SERVER
+    echo Submitted to queue: $PBS_QUEUE
+    echo Requested nodes per job: $PBS_NUM_NODES
+    echo Requested cores per node: $PBS_NUM_PPN
+    echo Requested cores per job: $PBS_NP
+    #echo Node list file: $PBS_NODEFILE
+    #echo Nodes assigned to job: $(cat $PBS_NODEFILE)
+    #echo Running node index: $PBS_O_NODENUM
+    echo
+    echo Running on hostname: $HOSTNAME
+    echo Parent PID: $PPID
+    echo Process PID: $$
+    echo Number of physical cores: $(grep "^core id" /proc/cpuinfo | sort -u | wc -l)
+    echo Number of virtual cores: $(nproc --all)
+    mem_report_cmd="free -g"
+    echo "Memory report [GB] ('${mem_report_cmd}'):"
+    echo -------------------------------------------------------------------------
+    free -g
+    echo -------------------------------------------------------------------------
+    echo
+    working_dir=$PBS_O_WORKDIR
 
 elif [ "$scheduler" == "SLURM" ]; then
-  echo ________________________________________
-  echo
-  echo SLURM Job Log
-  echo Start time: $(date)
-  echo
-  echo Job name: $SLURM_JOB_NAME
-  echo Job ID: $SLURM_JOBID
-  echo Submitted by user: $USER
-  echo User effective group ID: $(id -ng)
-  echo
-  echo SLURM account used: $SLURM_ACCOUNT
-  echo Hostname of submission: $SLURM_SUBMIT_HOST
-  echo Submitted to cluster: $SLURM_CLUSTER_NAME
-  echo Submitted to node: $SLURMD_NODENAME
-  echo Cores on node: $SLURM_CPUS_ON_NODE
-  echo Requested cores per task: $SLURM_CPUS_PER_TASK
-  echo Requested cores per job: $SLURM_NTASKS
-  echo Requested walltime: $SBATCH_TIMELIMIT
-  #echo Nodes assigned to job: $SLURM_JOB_NODELIST
-  #echo Running node index: $SLURM_NODEID
-  echo
-  echo Running on hostname: $HOSTNAME
-  echo Parent PID: $PPID
-  echo Process PID: $$
-  echo Number of physical cores: $(grep "^core id" /proc/cpuinfo | sort -u | wc -l)
-  echo Number of virtual cores: $(nproc --all)
-  mem_report_cmd="free -g"
-  echo "Memory report [GB] ('${mem_report_cmd}'):"
-  echo -------------------------------------------------------------------------
-  free -g
-  echo -------------------------------------------------------------------------
-  echo
-  working_dir=$SLURM_SUBMIT_DIR
+    echo ________________________________________
+    echo
+    echo SLURM Job Log
+    echo Start time: $(date)
+    echo
+    echo Job name: $SLURM_JOB_NAME
+    echo Job ID: $SLURM_JOBID
+    echo Submitted by user: $USER
+    echo User effective group ID: $(id -ng)
+    echo
+    echo SLURM account used: $SLURM_ACCOUNT
+    echo Hostname of submission: $SLURM_SUBMIT_HOST
+    echo Submitted to cluster: $SLURM_CLUSTER_NAME
+    echo Submitted to node: $SLURMD_NODENAME
+    echo Cores on node: $SLURM_CPUS_ON_NODE
+    echo Requested cores per task: $SLURM_CPUS_PER_TASK
+    echo Requested cores per job: $SLURM_NTASKS
+    echo Requested walltime: $SBATCH_TIMELIMIT
+    #echo Nodes assigned to job: $SLURM_JOB_NODELIST
+    #echo Running node index: $SLURM_NODEID
+    echo
+    echo Running on hostname: $HOSTNAME
+    echo Parent PID: $PPID
+    echo Process PID: $$
+    echo Number of physical cores: $(grep "^core id" /proc/cpuinfo | sort -u | wc -l)
+    echo Number of virtual cores: $(nproc --all)
+    mem_report_cmd="free -g"
+    echo "Memory report [GB] ('${mem_report_cmd}'):"
+    echo -------------------------------------------------------------------------
+    free -g
+    echo -------------------------------------------------------------------------
+    echo
+    working_dir=$SLURM_SUBMIT_DIR
 fi
 set -u
 
@@ -128,8 +128,8 @@ if [ ! -z "$py_ver_min" ]; then
         if (("${py_ver_nums[n]}" > "${py_ver_min_nums[n]}")); then
             break
         elif (("${py_ver_nums[n]}" < "${py_ver_min_nums[n]}")); then
-          echo "Python version (${py_ver}) is below accepted minimum (${py_ver_min}), exiting"
-          exit 1
+            echo "Python version (${py_ver}) is below accepted minimum (${py_ver_min}), exiting"
+            exit 1
         fi
     done
 fi
