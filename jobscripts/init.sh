9d2f07cf46937813d5324bc69669cb889b1ac09e
husby036 <husby036@umn.edu>
2020-06-05 13:56:14 -0500

    Updated structure of jobscript environment initialization

--- jobscripts/init.sh
+++ jobscripts/init.sh
@@ -6,24 +6,28 @@ set +u
 
 
 ### Handle environment loading ###
+default_env_load_cmd=''
 env_load_cmd=''
 
-# Here are some example commands that can load the environment needed by a batch script
+## Here are some example commands that can load the environment needed by a batch script
 #module load mod_name  # example load system module
 #source /path/to/env_root/bin/activate  # example load virtual environment
 #conda activate base  # example load conda environment
 
-# Modify the two code blocks below to set per-script and default environment load commands
+## Modify the two code blocks below to set default and per-script environment load commands
 
-## Set environment load commands specific to batch scripts
-#if [ "$job_class" == "s2s" ]; then
-#    env_load_cmd="module load mod_name"
-#fi
-
-## Set default environment load command for all batch scripts (fallback if no specific script setting)
-#default_env_load_cmd=''
+# Set default environment load command for all batch scripts (fallback if no specific script setting)
 #default_env_load_cmd="source /mnt/pgc/data/scratch/erik/build/miniconda3/bin/activate /mnt/pgc/data/scratch/erik/build/miniconda3/envs/s2s"
 #default_env_load_cmd="source /home/husby036/build/miniconda3/bin/activate /home/husby036/build/miniconda3/envs/s2s"
+
+# Set environment load commands specific to batch scripts
+if [ "$job_class" == "s2s" ]; then
+    env_load_cmd="$default_env_load_cmd"
+elif [ "$job_class" == "Mask" ]; then
+    env_load_cmd="$default_env_load_cmd"
+elif [ "$job_class" == "Check" ]; then
+    env_load_cmd="$default_env_load_cmd"
+fi
 
 
 # Don't modify the following two code blocks
