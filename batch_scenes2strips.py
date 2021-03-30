32bd9b8e14ffeb7c4a80e9a736c45ef4eb0f2a01
husby036 <husby036@umn.edu>
2021-03-30 15:49:56 -0500

    Add option to avoid using OpenCV for 50cm bitmask generation

--- batch_scenes2strips.py
+++ batch_scenes2strips.py
@@ -78,6 +78,7 @@ ARGSTR_DRYRUN = '--dryrun'
 ARGSTR_STRIPID = '--stripid'
 ARGSTR_SKIP_ORTHO2_ERROR = '--skip-xtrack-missing-ortho2-error'
 ARGSTR_SCENE_MASKS_ONLY = '--build-scene-masks-only'
+ARGSTR_USE_PIL_IMRESIZE = '--use-pil-imresize'
 
 # Argument choices
 ARGCHO_DEM_TYPE_LSF = 'lsf'
@@ -286,6 +287,17 @@ def argparser_init():
             "\n'{}': Filter designed specifically for 8m Antarctic DEMs.".format(ARGCHO_MASK_VER_REMA2A),
             "\n'{}': General-purpose filter for 8m DEMs.".format(ARGCHO_MASK_VER_MASK8M),
             "\n"
+        ])
+    )
+
+    parser.add_argument(
+        ARGSTR_USE_PIL_IMRESIZE,
+        action='store_true',
+        help=' '.join([
+            "Use PIL imresize method over usual fast OpenCV resize method for final resize from ",
+            "8m processing resolution back to native scene raster resolution when generating ",
+            "output bitmask raster. This is to avoid an OpenCV error with unknown cause that ",
+            "can occur when using OpenCV resize on some 50cm scenes at Blue Waters."
         ])
     )
 
@@ -1028,7 +1040,8 @@ def run_s2s(args, res_str, argcho_dem_type_opp, demSuffix):
             if not args.get(ARGSTR_DRYRUN):
                 generateMasks(scenedem_ffile, scene_mask_name, noentropy=args.get(ARGSTR_NOENTROPY),
                               save_component_masks=MASK_BIT, use_second_ortho=(stripid_is_xtrack and not bypass_ortho2),
-                              debug_component_masks=DEBUG_NONE, nbit_masks=False)
+                              debug_component_masks=DEBUG_NONE, nbit_masks=False,
+                              use_pil_imresize=args.get(ARGSTR_USE_PIL_IMRESIZE))
 
         if not args.get(ARGSTR_DRYRUN):
             print('')
