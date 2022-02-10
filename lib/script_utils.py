76cd350343665388faf9c509bc856e1d7d8ce121
husby036 <husby036@umn.edu>
2022-02-10 17:59:56 -0600

    Update script_utils.py VersionString class

--- lib/script_utils.py
+++ lib/script_utils.py
@@ -70,46 +70,115 @@ class ExternalError(Exception):
         super(Exception, self).__init__(msg)
 
 
-class VersionString:
-    def __init__(self, ver_str_or_num):
-        self.ver_str = str(ver_str_or_num)
-        self.ver_num_list = [int(n) for n in self.ver_str.split('.')]
-    def get_comparable_lists(self, other):
-        this_list = list(self.ver_num_list)
-        other_list = list(other.ver_num_list)
-        if len(this_list) < len(other_list):
-            this_list.extend([0]*(len(other_list)-len(this_list)))
-        elif len(this_list) > len(other_list):
-            other_list.extend([0]*(len(this_list)-len(other_list)))
-        return this_list, other_list
+class VersionString(object):
+    def __init__(self, version, nelements=None, allow_alpha=False):
+        self.is_numeric = (not allow_alpha)
+        if type(version) in (list, tuple):
+            nums_input = version
+            nums_internal = []
+            for n in nums_input:
+                if type(n) is float:
+                    n_float = n
+                    n_int = int(n)
+                    if n_float != n_int:
+                        raise InvalidArgumentError(
+                            "Non-integer element '{}' in version number list: {}".format(n, nums_input)
+                        )
+                    n = n_int
+                if self.is_numeric:
+                    try:
+                        n = int(n)
+                    except ValueError:
+                        raise InvalidArgumentError(
+                            "Non-numeric element '{}' in version number list: {}".format(n, nums_input)
+                        )
+                else:
+                    n = str(n).strip()
+                    if '.' in n:
+                        raise InvalidArgumentError(
+                            "Invalid element '{}' in version number list: {}".format(n, nums_input)
+                        )
+                nums_internal.append(n)
+            self.nums = nums_internal
+            self.string = '.'.join([str(n) for n in self.nums])
+        else:
+            version_string = version
+            self.string = str(version_string).strip()
+            if not self.is_numeric:
+                self.nums = [n.strip() for n in self.string.split('.')]
+            else:
+                nums_internal = []
+                for n in self.string.split('.'):
+                    try:
+                        n = int(n)
+                    except ValueError:
+                        raise InvalidArgumentError(
+                            "Non-numeric element '{}' in version string: '{}'".format(n, version_string)
+                        )
+                    nums_internal.append(n)
+                self.nums = nums_internal
+        if nelements is not None:
+            numel_diff = nelements - len(self.nums)
+            if numel_diff < 0:
+                raise InvalidArgumentError(
+                    "Provided version string '{}' has more elements ({}) than `nelements` ({})".format(
+                        self.string, len(self.nums), nelements
+                    )
+                )
+            elif numel_diff > 0:
+                self.nums.extend([0 if self.is_numeric else '0'] * numel_diff)
+                self.string = '.'.join([str(n) for n in self.nums])
     def __str__(self):
-        return self.ver_str
+        return self.string
     def __repr__(self):
-        return self.ver_str
-    def __compare_absolute(self, other, inequality=False):
-        this_ver_num_list, other_ver_num_list = self.get_comparable_lists(other)
-        for i in range(len(this_ver_num_list)):
-            if this_ver_num_list[i] != other_ver_num_list[i]:
+        return self.string
+    def _get_comparable_nums(self, other):
+        if self.is_numeric and other.is_numeric:
+            zero_num = 0
+            this_nums = list(self.nums)
+            other_nums = list(other.nums)
+        else:
+            zero_num = '0'
+            this_nums = [str(n) for n in self.nums] if self.is_numeric else list(self.nums)
+            other_nums = [str(n) for n in other.nums] if other.is_numeric else list(other.nums)
+        numel_diff = len(other_nums) - len(this_nums)
+        if numel_diff > 0:
+            this_nums.extend([zero_num] * numel_diff)
+        elif numel_diff < 0:
+            other_nums.extend([zero_num] * (-numel_diff))
+        if type(zero_num) is str:
+            for i in range(len(this_nums)):
+                ellen_diff = len(other_nums[i]) - len(this_nums[i])
+                if ellen_diff > 0:
+                    this_nums[i] = zero_num*ellen_diff + this_nums[i]
+                elif ellen_diff < 0:
+                    other_nums[i] = zero_num*(-ellen_diff) + other_nums[i]
+        return this_nums, other_nums
+    def _compare_absolute(self, other, inequality=False):
+        this_nums, other_nums = self._get_comparable_nums(other)
+        for i in range(len(this_nums)):
+            if this_nums[i] != other_nums[i]:
                 return inequality
         return (not inequality)
-    def __compare_relative(self, other, op, allow_equal=False):
-        this_ver_num_list, other_ver_num_list = self.get_comparable_lists(other)
-        for i in range(len(this_ver_num_list)):
-            if this_ver_num_list[i] != other_ver_num_list[i]:
-                return op(this_ver_num_list[i], other_ver_num_list[i])
+    def _compare_relative(self, other, op, allow_equal=False):
+        this_nums, other_nums = self._get_comparable_nums(other)
+        for i in range(len(this_nums)):
+            if this_nums[i] != other_nums[i]:
+                return op(this_nums[i], other_nums[i])
         return allow_equal
     def __eq__(self, other):
-        return self.__compare_absolute(other, inequality=False)
+        return self._compare_absolute(other, inequality=False)
     def __ne__(self, other):
-        return self.__compare_absolute(other, inequality=True)
+        return self._compare_absolute(other, inequality=True)
     def __gt__(self, other):
-        return self.__compare_relative(other, operator.gt, allow_equal=False)
+        return self._compare_relative(other, operator.gt, allow_equal=False)
     def __ge__(self, other):
-        return self.__compare_relative(other, operator.gt, allow_equal=True)
+        return self._compare_relative(other, operator.gt, allow_equal=True)
     def __lt__(self, other):
-        return self.__compare_relative(other, operator.lt, allow_equal=False)
+        return self._compare_relative(other, operator.lt, allow_equal=False)
     def __le__(self, other):
-        return self.__compare_relative(other, operator.le, allow_equal=True)
+        return self._compare_relative(other, operator.le, allow_equal=True)
+
 
 PYTHON_VERSION = VersionString(platform.python_version())
 if PYTHON_VERSION < VersionString(PYTHON_VERSION_REQUIRED_MIN):
